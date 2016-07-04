e13fb9ccd2acf8d77ca385f8f8750223d7b3f8d0
dongwenjuan <dong.wenjuan@zte.com.cn>
2016-07-04 08:47:59 +0800

    Update configure_mirror.sh

--- nodepool/scripts/configure_mirror.sh
+++ nodepool/scripts/configure_mirror.sh
@@ -23,7 +23,7 @@ source /usr/local/jenkins/slave_scripts/afs-slug.sh
 
 # set DNS address for openstack env
 OPENSTACK_DNS=172.10.0.1
-sudo echo "nameserver $OPENSTACK_DNS" >> /etc/resolv.conf
+echo "nameserver $OPENSTACK_DNS" |sudo tee -a /etc/resolv.conf
 
 NODEPOOL_MIRROR_HOST=http://mirrors.zte.com.cn
 
